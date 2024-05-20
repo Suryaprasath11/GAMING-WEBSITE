aeefd3c14acce48c4fea28680c429787ea322a55
Surya Prasath.S <163661707+Suryaprasath11@users.noreply.github.com>
2024-05-20 12:28:00 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,8 @@
+var sidenav = document.querySelector(".side-navbar")
+function showNavbar() {
+    sidenav.style.left = "0";
+    console.log("clicked")
+}
+function closeNavbar() {
+    sidenav.style.left = "-60%";
+}
